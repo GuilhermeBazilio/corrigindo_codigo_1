0347fdaabe9e09daaf9d24a53e8edc436d22ab0e
Guilherme Bazilio <bazilio.developer@gmail.com>
2021-09-07 11:00:41 -0300

    corrigindo o código

---
+++ src/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+  
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const {title, url, techs} = request.body;
+
+  const repository = repositories.find(repository => repository.id ==id);
+
+  if(!repository){
+    return response.status(404).json({"error":"Repository not found!"});
+  }
+
+  if(title!=null){
+    repository.title = title;
+  };
+    
+  if(url!=null){ 
+    repository.url = url;
+  };
+  if(techs!=null){ 
+    repository.techs = techs;
+  };
+
+  return response.json(repository);
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex == -1) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex == -1) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const repository = repositories.find(repository => repository.id ==id);
+
+  const likes = repository.likes;
+  
+  repository.likes = likes+1;
+
+  return response.json(repository);
+});
+
+//app.listen(3333);
+module.exports = app;
